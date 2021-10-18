66b00cd5f89f2d114212d987e61780755278bef6
jmeirink <84925553+jmeirink@users.noreply.github.com>
2021-10-18 17:00:20 -0500

    Added project files

---
+++ game.js
@@ -0,0 +1,43 @@
+
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+const gamePattern = [];
+
+const userClickedPattern = [];
+
+
+
+
+
+
+
+$(".btn").click(function() {
+  const userChosenColour = $(this).attr("id");
+});
+
+userClickedPattern.push(userChosenColour);
+
+console.log(userClickedPattern);
+
+
+
+
+
+
+
+function nextSequence() {
+  const randomNumber = Math.floor(Math.random() * 4);
+
+  return randomNumber;
+};
+
+const randomChosenColour = buttonColours[nextSequence()];
+
+gamePattern.push(randomChosenColour);
+
+$("#" + randomChosenColour).fadeOut(100).fadeIn(100)
+
+///////////// Not working in chrome, but does work in Safari
+const audio = new Audio('sounds/' + randomChosenColour + '.mp3');
+audio.play();
+/////////////
